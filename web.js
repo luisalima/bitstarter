3d93c33d5fdb1b8244c60fcc4be8fc8fddb64724
Luisa Lima <luisalima@dcc.fc.up.pt>
2013-07-11 22:00:24 +0100

    Read message from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var str = new Buffer(fs.readFileSync("./index.html"), "utf-8").toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
